180a09fb7a96470a2b776994b085034fc3a0e2ea
Leon <143452196+leonbennazar@users.noreply.github.com>
2025-10-24 18:45:54 -0300

    about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,40 @@
 <script>
+	import { goto } from "$app/navigation";
 	import Navbar from "$lib/components/navbar.svelte";
 
 </script>
 <Navbar></Navbar>
+
+<div class= "content">
+	
+	<div class="container">
+		<button on:click={()=> goto('/about')}>Sobre nosotros</button>
+	</div>
+
+</div>
+
+
+
+<style>
+	.content{
+		margin-top: 70px;
+	}
+
+	.container{
+		display:flex;
+		justify-content: center;
+	}
+
+	button{
+		font-size: 20px;
+		background-color:#2c2d83;
+		border-radius: 5px;
+		border: none;
+		color: white;
+	}
+
+  button:hover {
+    color: #5f69f5;
+  }
+
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Navbar from "$lib/components/navbar.svelte";
+</script>
+
+<Navbar></Navbar>
+
+<div class = "content">
+
+</div>
+
+<style>
+  .content{
+    margin-top:70px;
+  }
+
+
+</style>
